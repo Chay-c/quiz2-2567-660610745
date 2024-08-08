ebe3f906764ed74b133154debff6bca7406965ef
Chay <chayangkul_c@cmu.ac.th>
2024-08-08 14:57:00 +0700

    1st

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,15 +1,37 @@
 "use client";
 export default function Comment({
-  ImagePath,
+  userImagePath,
   username,
-  commentTitle,
-  likes,
+  commentText,
+  likeNum ,
   replies,
-}) {
+}:any) {
   return (
-    <div>
-      your code for Comment component here ...
-      {/* You can use map-loop to render Reply component here */}
+    <div className="d-flex gap-2 my-2">
+    <img
+      src="/profileImages/lisa.jpg"
+      width="48"
+      height="48"
+      className="rounded-circle"
+      style={{ objectFit: "cover" }}
+    />{userImagePath}
+    <div
+      className="rounded rounded-3 p-2"
+      style={{ backgroundColor: "#E5E7EB" }}
+    >
+      <span className="fw-semibold">Lisa</span>{username}
+      <br />
+      <span>จริงค่า</span> {commentText}
+      <div className="d-flex align-items-center gap-1">
+        <img src="./like.svg" width={20}></img> {likeNum }
+        <span className="text-muted">999 คน</span>{replies}
+      </div>
     </div>
+  </div>
+
+
+
+  
   );
+  
 }
--- src/components/Reply.tsx
+++ src/components/Reply.tsx
@@ -1,5 +1,33 @@
 "use client";
 
-export default function Reply({ ImagePath, username, replyTitle, likes }) {
-  return <div>your code for Reply component ...</div>;
+export default function Reply({
+  userImagePath ,
+  username, 
+  replyText, 
+  likeNum 
+}:any) {
+
+  return ( <div className="d-flex gap-2 my-2 ps-5">
+  <img
+    src="/profileImages/puppy.jpg"
+    width="48"
+    height="48"
+    className="rounded-circle"
+    style={{ objectFit: "cover" }}
+  />{userImagePath }
+  <div
+    className="rounded rounded-3 p-2"
+    style={{ backgroundColor: "#E5E7EB" }}
+  >
+    <span className="fw-semibold">หมาน้อย</span> {username}
+    <br />
+    <span>จริงค้าบบบบบบบบ</span>{replyText}
+    <div className="d-flex align-items-center gap-1">
+      <img src="./like.svg" width={20}></img>{likeNum}
+      <span className="text-muted">2 คน</span>
+    </div>
+  </div>
+</div>
+);
 }
+
